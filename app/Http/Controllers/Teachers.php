46b918b197b64538746fe614fa09e23107d2e666
Chipskein <abfn0905@gmail.com>
2022-06-04 22:11:10 -0300

    prepare routes

--- app/Http/Controllers/Teachers.php
+++ app/Http/Controllers/Teachers.php
@@ -13,7 +13,7 @@ class Teachers extends Controller
      */
     public function index()
     {
-        //
+        return view('test');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Avaliations;
+use App\Http\Controllers\Classes;
+use App\Http\Controllers\Disciplines;
+use App\Http\Controllers\Students;
+use App\Http\Controllers\Teachers;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +18,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/',"App\Http\Controllers\PageController@showHome");
+Route::controller(Teachers::class)->group(function () {
+    Route::get("/teachers", "index");
+    Route::get("/teachers/{id}", "show");
+    Route::post("/teachers","create");
 });
+Route::controller(Students::class)->group(function () {
+    Route::get("/students", "index");
+    Route::get("/students/{id}", "show");
+    Route::post("/students","create");
+});
+Route::controller(Disciplines::class)->group(function () {
+    Route::get("/disciplines", "index");
+    Route::get("/disciplines/{id}", "show");
+    Route::post("/disciplines","create");
+});
+Route::controller(Classes::class)->group(function () {
+    Route::get("/classes", "index");
+    Route::get("/classes/{id}", "show");
+    Route::post("/classes","create");
+});
+Route::controller(Avaliations::class)->group(function () {
+    Route::get("/avaliations", "index");
+    Route::get("/avaliations/{id}", "show");
+    Route::post("/avaliations","create");
+});
+
+
