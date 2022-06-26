89ff485e71b4127162e5aea86699aed133d0778f
Silvio <silvioquintana1@hotmail.com>
2022-06-26 19:38:17 -0300

    css brunao

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,50 @@
+<head>
+<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <style>
+    header {
+        background-color: #5C9F5C;
+        width: 100%;
+        padding:0px;
+        margin:0px;
+    }
+    body {
+        margin:0px;
+        padding:0px;
+    }
+    .header {
+        display: flex;
+        justify-content: space-between;
+        display: flex;
+        align-items: center;
+        padding:0px 50px 0px 50px;
+    }
+    .headerNav {
+        display: flex;
+        align-items: center;
+    }
+    .navItem {
+        margin-left: 20px;
+        font-size: 18px;
+        color:white;
+        cursor:pointer
+    }
+    .headerTitle {
+        font-size: 32px;
+        color:white;
+    }
+        </style>
+</head>
+<header>
+    <div class='header'>
+    <p class='headerTitle'>Sistema acadêmico</p>
+    <div class='headerNav'>
+        <a class='navItem'>Alunos</a>
+        <a class='navItem'>Professores</a>
+        <a class='navItem'>Disciplinas</a>
+        <a class='navItem'>Turmas</a>
+    </div>
+    </div>
+</header>
