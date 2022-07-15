26108932af59c3938c88527063460f112f0cd9d4
Chipskein <abfn0905@gmail.com>
2022-07-14 22:16:53 -0300

    testando

--- app/Models/Disciplines_Students.php
+++ app/Models/Disciplines_Students.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Facades\DB;
 class Disciplines_Students extends Model
 {
     use HasFactory;
     protected $table='Disciplines_Students';
+    public $timestamps=false;
     protected $fillable = [
         'discipline',
         'student',
